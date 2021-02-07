6db14592b03fa8f983c2e8d1e31aab6a1b53242d
Manuel Perez <manuel.perez2015@gmail.com>
2021-02-07 15:30:46 -0800

    Made the slideshow fully responsive and functional.

--- scripts/js/index.js
+++ scripts/js/index.js
@@ -21,3 +21,39 @@ const navSlide = () => {
     });
 }
 navSlide();
+
+function sliderheight() {
+    
+    const clientHeight = document.getElementById('slide').clientHeight;
+
+    document.getElementById("slide-container").style.height = `${clientHeight}px`;
+
+    window.addEventListener("resize", sliderheight);
+}
+sliderheight();
+
+
+const slides = document.querySelectorAll('.slide');
+const buttons = document.querySelectorAll('.button');
+let currSlide = 0;
+
+var manualNav = function(man) {
+    slides.forEach((slide) => {
+        slide.classList.remove('active');
+
+        buttons.forEach((button) => {
+            button.classList.remove('active');
+        });
+    });
+
+    slides[man].classList.add('active');
+    buttons[man].classList.add('active');
+
+    buttons.forEach((button, curr) => {
+        button.addEventListener('click', () => {
+            manualNav(curr);
+            currSlide = curr;
+        });
+    });
+}
+manualNav(0);
