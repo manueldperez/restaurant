b41045da5672b2b4f66f2e1616a10dd95679c107
Manuel Perez <manuel.perez2015@gmail.com>
2021-06-05 18:15:06 -0700

    Set a max width for larger screens. Also added a favicon.

--- scripts/js/photos.js
+++ scripts/js/photos.js
@@ -1,76 +1,76 @@
-window.addEventListener('load', () => {
-    const clientHeight = document.getElementById('slide').clientHeight;
-    document.getElementById("slide-container").style.height = `${clientHeight}px`;
-    document.getElementById("slide-container").style.transition = 'all .2s ease-in-out';
-})
+window.addEventListener("load", () => {
+  const clientHeight = document.getElementById("slide").clientHeight;
+  document.getElementById("slide-container").style.height = `${clientHeight}px`;
+  document.getElementById("slide-container").style.transition =
+    "all .2s ease-in-out";
+});
 
 // function to calculate the height of slideshow
 function sliderheight() {
-    const clientHeight = document.getElementById('slide').clientHeight;
-    document.getElementById("slide-container").style.height = `${clientHeight}px`;
-    window.addEventListener("resize", sliderheight);
-    if (document.getElementById("slide-container").style.transition !== null) {
-        document.getElementById("slide-container").style.transition = null;
-    }
+  const clientHeight = document.getElementById("slide").clientHeight;
+  document.getElementById("slide-container").style.height = `${clientHeight}px`;
+  window.addEventListener("resize", sliderheight);
+  if (document.getElementById("slide-container").style.transition !== null) {
+    document.getElementById("slide-container").style.transition = null;
+  }
 }
 sliderheight();
 
-const slides = document.querySelectorAll('.slide');
-const buttons = document.querySelectorAll('.button');
+const slides = document.querySelectorAll(".slide");
+const buttons = document.querySelectorAll(".button");
 let currSlide = 0;
 var timer;
 
-const manualNav = (man) => {
-    slides.forEach((slide) => {
-        slide.classList.remove('active');
+const manualNav = man => {
+  slides.forEach(slide => {
+    slide.classList.remove("active");
 
-        buttons.forEach((button) => {
-            button.classList.remove('active');
-        });
+    buttons.forEach(button => {
+      button.classList.remove("active");
     });
-    slides[man].classList.add('active');
-    buttons[man].classList.add('active');
+  });
+  slides[man].classList.add("active");
+  buttons[man].classList.add("active");
 
-    buttons.forEach((button, curr) => {
-        button.addEventListener('click', () => {
-            manualNav(curr);
-            currSlide = curr;
-            clearTimeout(timer);
-            repeat();
-        });
+  buttons.forEach((button, curr) => {
+    button.addEventListener("click", () => {
+      manualNav(curr);
+      currSlide = curr;
+      clearTimeout(timer);
+      repeat();
     });
-}
+  });
+};
 manualNav(currSlide);
 
 const repeat = () => {
-    let curr = null;
-    if (currSlide == 4) {
-        curr = currSlide;
-    }
-    else {
-        curr = currSlide + 1;
-    }
-    const repeater = () => {
-        timer = setTimeout(() => {
-            slides.forEach((slide) => {
-                slide.classList.remove('active');
-        
-                buttons.forEach((button) => {
-                    button.classList.remove('active');
-                });
-            });
-            if (slides.length == curr) {
-                curr = 0;
-            }
-            if (curr >= slides.length) {
-                return;
-            }
-            slides[curr].classList.add('active');
-            buttons[curr].classList.add('active');
-            curr++;
-            repeater();
-        }, 5000);
-    }
-    repeater();
-}
+  let curr = null;
+  if (currSlide == 4) {
+    curr = currSlide;
+  } else {
+    curr = currSlide + 1;
+  }
+  const repeater = () => {
+    timer = setTimeout(() => {
+      slides.forEach(slide => {
+        slide.classList.remove("active");
+
+        buttons.forEach(button => {
+          button.classList.remove("active");
+        });
+      });
+      if (slides.length == curr) {
+        curr = 0;
+      }
+      if (curr >= slides.length) {
+        return;
+      }
+      slides[curr].classList.add("active");
+      buttons[curr].classList.add("active");
+      curr++;
+      repeater();
+    }, 5000);
+  };
+  repeater();
+};
 repeat();
